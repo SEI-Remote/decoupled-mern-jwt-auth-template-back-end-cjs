e36aa5f5e961c1a732b3855e90aa6b5be107d44d
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-01-19 20:03:48 -0600

    add annotations to server

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
+// require npm packages
 require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const formData = require('express-form-data')
 
+// connect to MongoDB with mongoose
+require('./config/database.js')
+
+// require routes
 const profilesRouter = require('./routes/profiles.js').router
 const authRouter = require('./routes/auth.js').router
 
-require('./config/database.js')
-
+// create the express app
 const app = express()
 
+// basic middleware
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(formData.parse())
 
+// router middleware
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 
+// handle 404 errors
 app.use(function (req, res, next) {
   res.status(404).json({ err: 'Not found' })
 })
 
+// handle all other errors
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json({ err: err.message })
 })
